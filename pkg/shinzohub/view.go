eb6c79823d3ffdde7f6422fb47b516dcf3b23166
Quinn Purdy <quinn.purdy.gamedev@gmail.com>
2025-10-15 11:18:27 -0400

    Added event subscription logic

---
+++ pkg/shinzohub/view.go
@@ -0,0 +1,39 @@
+package shinzohub
+
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/shinzonetwork/host/pkg/view"
+)
+
+type ViewRegisteredEvent struct { // ViewRegisteredEvent implements ShinzoEvent interface
+	Key     string
+	Creator string
+	View    view.View
+}
+
+func (event *ViewRegisteredEvent) ToString() string {
+	return fmt.Sprintf("(Key: %s, Creator: %s, View: %s)", event.Key, event.Creator, event.View.Name)
+}
+
+// ExtractNameFromSDL extracts the type name from the SDL string
+func ExtractNameFromSDL(view *view.View) {
+	if view.Sdl == nil {
+		view.Name = ""
+		return
+	}
+	// Look for pattern: type <Name> @...
+	re := regexp.MustCompile(`type\s+(\w+)\s+@`)
+	matches := re.FindStringSubmatch(*view.Sdl)
+	if len(matches) > 1 {
+		view.Name = matches[1]
+	} else {
+		// Fallback: look for type <Name> { pattern
+		re = regexp.MustCompile(`type\s+(\w+)\s+{`)
+		matches = re.FindStringSubmatch(*view.Sdl)
+		if len(matches) > 1 {
+			view.Name = matches[1]
+		}
+	}
+}
