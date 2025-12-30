565f23ca5c2b2b96294f4daee28d4504b6b976f0
dvncan <dunbrown64@gmail.com>
2025-12-30 14:55:58 -0500

    chore: fix graphql tests

--- pkg/graphql/addBlockNumberFilterToQuery_test.go
+++ pkg/graphql/addBlockNumberFilterToQuery_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/shinzonetwork/shinzo-host-client/pkg/constants"
 	"github.com/stretchr/testify/require"
 )
 
@@ -18,58 +19,58 @@ func TestAddBlockNumberFilter(t *testing.T) {
 	}{
 		{
 			name:          "simple query without filter",
-			query:         "Log { address topics data transactionHash }",
+			query:         constants.CollectionLog + " { address topics data transactionHash }",
 			startingBlock: 1000,
 			endingBlock:   9999999,
-			expectedQuery: "Log (filter: { _and: [ { blockNumber: { _ge: 1000 } }, { blockNumber: { _le: 9999999 } } ] }, order: { blockNumber: DESC }){ address topics data transactionHash }",
+			expectedQuery: constants.CollectionLog + " (filter: { _and: [ { blockNumber: { _ge: 1000 } }, { blockNumber: { _le: 9999999 } } ] }, order: { blockNumber: DESC }){ address topics data transactionHash }",
 			expectError:   false,
 		},
 		{
 			name:          "query with existing filter",
-			query:         "Log(filter: { address: { _eq: \"0x123\" } }) { address topics data }",
+			query:         constants.CollectionLog + "(filter: { address: { _eq: \"0x123\" } }) { address topics data }",
 			startingBlock: 2000,
 			endingBlock:   5000,
-			expectedQuery: "Log(filter: { address: { _eq: \"0x123\" } }, _and: [ { blockNumber: { _ge: 2000 } }, { blockNumber: { _le: 5000 } } ], order: { blockNumber: DESC }) { address topics data }",
+			expectedQuery: constants.CollectionLog + "(filter: { address: { _eq: \"0x123\" } }, _and: [ { blockNumber: { _ge: 2000 } }, { blockNumber: { _le: 5000 } } ], order: { blockNumber: DESC }) { address topics data }",
 			expectError:   false,
 		},
 		{
 			name:          "query with empty existing filter",
-			query:         "Log(filter: { }) { address topics data }",
+			query:         constants.CollectionLog + "(filter: { }) { address topics data }",
 			startingBlock: 3000,
 			endingBlock:   6000,
-			expectedQuery: "Log(filter: { }, _and: [ { blockNumber: { _ge: 3000 } }, { blockNumber: { _le: 6000 } } ], order: { blockNumber: DESC }) { address topics data }",
+			expectedQuery: constants.CollectionLog + "(filter: { }, _and: [ { blockNumber: { _ge: 3000 } }, { blockNumber: { _le: 6000 } } ], order: { blockNumber: DESC }) { address topics data }",
 			expectError:   false,
 		},
 		{
 			name:          "transaction query without filter",
-			query:         "Transaction { hash from to value }",
+			query:         constants.CollectionTransaction + " { hash from to value }",
 			startingBlock: 1500,
 			endingBlock:   8000,
-			expectedQuery: "Transaction (filter: { _and: [ { blockNumber: { _ge: 1500 } }, { blockNumber: { _le: 8000 } } ] }, order: { blockNumber: DESC }){ hash from to value }",
+			expectedQuery: constants.CollectionTransaction + " (filter: { _and: [ { blockNumber: { _ge: 1500 } }, { blockNumber: { _le: 8000 } } ] }, order: { blockNumber: DESC }){ hash from to value }",
 			expectError:   false,
 		},
 		{
 			name:          "block query without filter",
-			query:         "Block { hash number timestamp }",
+			query:         constants.CollectionBlock + " { hash number timestamp }",
 			startingBlock: 5000,
 			endingBlock:   10000,
-			expectedQuery: "Block (filter: { _and: [ { number: { _ge: 5000 } }, { number: { _le: 10000 } } ] }, order: { number: DESC }){ hash number timestamp }",
+			expectedQuery: constants.CollectionBlock + " (filter: { _and: [ { number: { _ge: 5000 } }, { number: { _le: 10000 } } ] }, order: { number: DESC }){ hash number timestamp }",
 			expectError:   false,
 		},
 		{
 			name:          "access list entry query without filter",
-			query:         "AccessListEntry { address storageKeys }",
+			query:         constants.CollectionAccessListEntry + " { address storageKeys }",
 			startingBlock: 1000,
 			endingBlock:   2000,
-			expectedQuery: "AccessListEntry (filter: { _and: [ { transaction: { blockNumber: { _ge: 1000 } } }, { transaction: { blockNumber: { _le: 2000 } } } ] }, order: { transaction: { blockNumber: DESC } }){ address storageKeys }",
+			expectedQuery: constants.CollectionAccessListEntry + " (filter: { _and: [ { transaction: { blockNumber: { _ge: 1000 } } }, { transaction: { blockNumber: { _le: 2000 } } } ] }, order: { transaction: { blockNumber: DESC } }){ address storageKeys }",
 			expectError:   false,
 		},
 		{
 			name:          "query with whitespace",
-			query:         "  Log  {  address  topics  }  ",
+			query:         constants.CollectionLog + "  {  address  topics  }  ",
 			startingBlock: 100,
 			endingBlock:   500,
-			expectedQuery: "Log  (filter: { _and: [ { blockNumber: { _ge: 100 } }, { blockNumber: { _le: 500 } } ] }, order: { blockNumber: DESC }){  address  topics  }",
+			expectedQuery: constants.CollectionLog + "  (filter: { _and: [ { blockNumber: { _ge: 100 } }, { blockNumber: { _le: 500 } } ] }, order: { blockNumber: DESC }){  address  topics  }",
 			expectError:   false,
 		},
 		{
@@ -84,7 +85,7 @@ func TestAddBlockNumberFilter(t *testing.T) {
 		},
 		{
 			name:        "query without selection set",
-			query:       "Log",
+			query:       constants.CollectionLog,
 			expectError: true,
 		},
 	}
@@ -114,31 +115,31 @@ func TestAddBlockNumberFilter_ComplexFilters(t *testing.T) {
 	}{
 		{
 			name:          "multiple existing filters",
-			query:         "Log(filter: { address: { _eq: \"0x123\" }, topics: { _in: [\"0xabc\"] } }) { address topics data }",
+			query:         constants.CollectionLog + "(filter: { address: { _eq: \"0x123\" }, topics: { _in: [\"0xabc\"] } }) { address topics data }",
 			startingBlock: 4000,
 			endingBlock:   7000,
-			expectedQuery: "Log(filter: { address: { _eq: \"0x123\" }, topics: { _in: [\"0xabc\"] } }, _and: [ { blockNumber: { _ge: 4000 } }, { blockNumber: { _le: 7000 } } ], order: { blockNumber: DESC }) { address topics data }",
+			expectedQuery: constants.CollectionLog + "(filter: { address: { _eq: \"0x123\" }, topics: { _in: [\"0xabc\"] } }, _and: [ { blockNumber: { _ge: 4000 } }, { blockNumber: { _le: 7000 } } ], order: { blockNumber: DESC }) { address topics data }",
 		},
 		{
 			name:          "nested filter structure",
-			query:         "Log(filter: { address: { _eq: \"0x123\" }, _and: [{ topics: { _in: [\"0xabc\"] } }] }) { address topics data }",
+			query:         constants.CollectionLog + "(filter: { address: { _eq: \"0x123\" }, _and: [{ topics: { _in: [\"0xabc\"] } }] }) { address topics data }",
 			startingBlock: 5000,
 			endingBlock:   9000,
-			expectedQuery: "Log(filter: { address: { _eq: \"0x123\" }, _and: [{ topics: { _in: [\"0xabc\"] } }] }, _and: [ { blockNumber: { _ge: 5000 } }, { blockNumber: { _le: 9000 } } ], order: { blockNumber: DESC }) { address topics data }",
+			expectedQuery: constants.CollectionLog + "(filter: { address: { _eq: \"0x123\" }, _and: [{ topics: { _in: [\"0xabc\"] } }] }, _and: [ { blockNumber: { _ge: 5000 } }, { blockNumber: { _le: 9000 } } ], order: { blockNumber: DESC }) { address topics data }",
 		},
 		{
 			name:          "access list entry with existing filter",
-			query:         "AccessListEntry(filter: { address: { _eq: \"0x456\" } }) { address storageKeys }",
+			query:         constants.CollectionAccessListEntry + "(filter: { address: { _eq: \"0x456\" } }) { address storageKeys }",
 			startingBlock: 1000,
 			endingBlock:   3000,
-			expectedQuery: "AccessListEntry(filter: { address: { _eq: \"0x456\" } }, _and: [ { transaction: { blockNumber: { _ge: 1000 } } }, { transaction: { blockNumber: { _le: 3000 } } } ], order: { transaction: { blockNumber: DESC } }) { address storageKeys }",
+			expectedQuery: constants.CollectionAccessListEntry + "(filter: { address: { _eq: \"0x456\" } }, _and: [ { transaction: { blockNumber: { _ge: 1000 } } }, { transaction: { blockNumber: { _le: 3000 } } } ], order: { transaction: { blockNumber: DESC } }) { address storageKeys }",
 		},
 		{
 			name:          "block query with existing filter",
-			query:         "Block(filter: { hash: { _eq: \"0x789\" } }) { hash number timestamp }",
+			query:         constants.CollectionBlock + "(filter: { hash: { _eq: \"0x789\" } }) { hash number timestamp }",
 			startingBlock: 2000,
 			endingBlock:   4000,
-			expectedQuery: "Block(filter: { hash: { _eq: \"0x789\" } }, _and: [ { number: { _ge: 2000 } }, { number: { _le: 4000 } } ], order: { number: DESC }) { hash number timestamp }",
+			expectedQuery: constants.CollectionBlock + "(filter: { hash: { _eq: \"0x789\" } }, _and: [ { number: { _ge: 2000 } }, { number: { _le: 4000 } } ], order: { number: DESC }) { hash number timestamp }",
 		},
 	}
 
@@ -162,42 +163,42 @@ func TestAddBlockNumberFilter_EdgeCases(t *testing.T) {
 	}{
 		{
 			name:          "same starting and ending block",
-			query:         "Log { address topics }",
+			query:         constants.CollectionLog + " { address topics }",
 			startingBlock: 1000,
 			endingBlock:   1000,
-			expectedQuery: "Log (filter: { _and: [ { blockNumber: { _ge: 1000 } }, { blockNumber: { _le: 1000 } } ] }, order: { blockNumber: DESC }){ address topics }",
+			expectedQuery: constants.CollectionLog + " (filter: { _and: [ { blockNumber: { _ge: 1000 } }, { blockNumber: { _le: 1000 } } ] }, order: { blockNumber: DESC }){ address topics }",
 			expectError:   false,
 		},
 		{
 			name:          "ending block less than starting block",
-			query:         "Log { address topics }",
+			query:         constants.CollectionLog + " { address topics }",
 			startingBlock: 5000,
 			endingBlock:   1000,
-			expectedQuery: "Log (filter: { _and: [ { blockNumber: { _ge: 5000 } }, { blockNumber: { _le: 1000 } } ] }, order: { blockNumber: DESC }){ address topics }",
+			expectedQuery: constants.CollectionLog + " (filter: { _and: [ { blockNumber: { _ge: 5000 } }, { blockNumber: { _le: 1000 } } ] }, order: { blockNumber: DESC }){ address topics }",
 			expectError:   false,
 		},
 		{
 			name:          "zero starting block",
-			query:         "Log { address topics }",
+			query:         constants.CollectionLog + " { address topics }",
 			startingBlock: 0,
 			endingBlock:   1000,
-			expectedQuery: "Log (filter: { _and: [ { blockNumber: { _ge: 0 } }, { blockNumber: { _le: 1000 } } ] }, order: { blockNumber: DESC }){ address topics }",
+			expectedQuery: constants.CollectionLog + " (filter: { _and: [ { blockNumber: { _ge: 0 } }, { blockNumber: { _le: 1000 } } ] }, order: { blockNumber: DESC }){ address topics }",
 			expectError:   false,
 		},
 		{
 			name:          "large block numbers",
-			query:         "Log { address topics }",
+			query:         constants.CollectionLog + " { address topics }",
 			startingBlock: 18446744073709551615, // max uint64
 			endingBlock:   18446744073709551615,
-			expectedQuery: "Log (filter: { _and: [ { blockNumber: { _ge: 18446744073709551615 } }, { blockNumber: { _le: 18446744073709551615 } } ] }, order: { blockNumber: DESC }){ address topics }",
+			expectedQuery: constants.CollectionLog + " (filter: { _and: [ { blockNumber: { _ge: 18446744073709551615 } }, { blockNumber: { _le: 18446744073709551615 } } ] }, order: { blockNumber: DESC }){ address topics }",
 			expectError:   false,
 		},
 		{
 			name:          "transaction with complex existing filter",
-			query:         "Transaction(filter: { _and: [{ from: { _eq: \"0x123\" } }, { to: { _eq: \"0x456\" } }] }) { hash from to }",
+			query:         constants.CollectionTransaction + "(filter: { _and: [{ from: { _eq: \"0x123\" } }, { to: { _eq: \"0x456\" } }] }) { hash from to }",
 			startingBlock: 1000,
 			endingBlock:   2000,
-			expectedQuery: "Transaction(filter: { _and: [{ from: { _eq: \"0x123\" } }, { to: { _eq: \"0x456\" } }] }, _and: [ { blockNumber: { _ge: 1000 } }, { blockNumber: { _le: 2000 } } ], order: { blockNumber: DESC }) { hash from to }",
+			expectedQuery: constants.CollectionTransaction + "(filter: { _and: [{ from: { _eq: \"0x123\" } }, { to: { _eq: \"0x456\" } }] }, _and: [ { blockNumber: { _ge: 1000 } }, { blockNumber: { _le: 2000 } } ], order: { blockNumber: DESC }) { hash from to }",
 			expectError:   false,
 		},
 	}
