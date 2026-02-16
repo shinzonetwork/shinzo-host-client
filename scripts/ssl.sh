edfcc91ec53a5b00b852dfeb97d5de784a34bc1f
dvncan <dunbrown64@gmail.com>
2026-02-16 12:10:49 -0500

    pushing

---
+++ scripts/ssl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+# SSL Certificate
+# Create SSL directory
+sudo mkdir -p ~/ssl
+
+# Generate private key
+sudo openssl genrsa -out ~/ssl/nginx.key 2048
+
+# Generate certificate signing request
+sudo openssl req -new -key ~/ssl/nginx.key -out /tmp/nginx.csr -subj "/C=US/ST=State/L=City/O=Shinzo/OU=Host Client/CN=$DOMAIN"
+
+# Generate self-signed certificate (valid for 365 days)
+sudo openssl x509 -req -days 365 -in /tmp/nginx.csr -signkey ~/ssl/nginx.key -out ~/ssl/nginx.crt
