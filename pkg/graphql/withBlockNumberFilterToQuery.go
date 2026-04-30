c7ce0d5bef738cabbd5dd6827847f8447c54c67c
dvncan <dunbrown64@gmail.com>
2026-04-30 17:14:21 -0400

    chore: fix graphql tests

--- pkg/graphql/withBlockNumberFilterToQuery.go
+++ pkg/graphql/withBlockNumberFilterToQuery.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"regexp"
 	"strings"
+
+	"github.com/shinzonetwork/shinzo-host-client/pkg/constants"
 )
 
 // WithBlockNumberFilter adds a block number filter to a GraphQL query.
@@ -19,10 +21,25 @@ func WithBlockNumberFilter(query string, startingBlockNumber uint64, endingBlock
 		return "", fmt.Errorf("query %q: %w", query, ErrParseCollectionName)
 	}
 
-	cfg := getCollectionFilterConfig(matches[1], startingBlockNumber, endingBlockNumber)
+	cfg := collectionFilterConfig{
+		rangeFilter: withCollectionRangeFilter(matches[1], startingBlockNumber, endingBlockNumber),
+		orderClause: "", // WithBlockNumberFilter does not add ordering
+	}
 
 	if matches[2] == "(" {
 		return spliceIntoExistingFilter(trimmed, cfg)
 	}
 	return insertNewFilter(trimmed, cfg)
+}
+
+// withCollectionRangeFilter returns only the range filter string for WithBlockNumberFilter (no order clause).
+func withCollectionRangeFilter(collectionName string, start, end uint64) string {
+	switch collectionName {
+	case constants.CollectionBlock:
+		return fmt.Sprintf("_and: [ { number: { _ge: %d } }, { number: { _le: %d } } ]", start, end)
+	case constants.CollectionAccessListEntry:
+		return fmt.Sprintf("_and: [ { transaction: { blockNumber: { _ge: %d } } }, { transaction: { blockNumber: { _le: %d } } } ]", start, end)
+	default:
+		return fmt.Sprintf("_and: [ { blockNumber: { _ge: %d } }, { blockNumber: { _le: %d } } ]", start, end)
+	}
 }
