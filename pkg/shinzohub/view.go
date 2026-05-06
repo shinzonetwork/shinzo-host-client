3eb6e7d11de84f2c7a7dd5035da8fbc680c23c24
dvncan <dunbrown64@gmail.com>
2026-05-06 18:51:35 -0400

    chore: remove weird comments

--- pkg/shinzohub/view.go
+++ pkg/shinzohub/view.go
@@ -9,28 +9,28 @@ import (
 // ViewRegisteredEvent represents a view registration event from ShinzoHub.
 // Emitted by ViewRegistry precompile (0x210) as Cosmos SDK event "ViewRegistered".
 type ViewRegisteredEvent struct {
-	ViewAddress string    // attr "view_address": hex EVM contract address of deployed View.sol
-	ViewName    string    // attr "view_name": SDL resource name extracted from viewbundle
-	Creator     string    // attr "creator": bech32 address of the view creator
-	View        view.View // attr "data": decoded from base64 viewbundle wire format
+	ViewAddress string
+	ViewName    string
+	Creator     string
+	View        view.View
 }
 
 // HostRegisteredEvent represents a host registration event from ShinzoHub.
 // Emitted by HostRegistry precompile (0x211) as Cosmos SDK event "HostRegistered".
 type HostRegisteredEvent struct {
-	Owner            string // attr "owner": bech32 address of host operator
-	DID              string // attr "did": decentralized identifier derived from registration pubkey
-	ConnectionString string // attr "connection_string": network address (multiaddr or ip:port)
+	Owner            string
+	DID              string
+	ConnectionString string
 }
 
 // IndexerRegisteredEvent represents an indexer registration event from ShinzoHub.
 // Emitted by IndexerRegistry precompile (0x212) as Cosmos SDK event "IndexerRegistered".
 type IndexerRegisteredEvent struct {
-	Owner            string // attr "owner": bech32 address of indexer operator
-	DID              string // attr "did": decentralized identifier derived from registration pubkey
-	ConnectionString string // attr "connection_string": network address (multiaddr or ip:port)
-	SourceChain      string // attr "source_chain": name of indexed chain (e.g., "ethereum")
-	SourceChainID    string // attr "source_chain_id": chain ID as string (e.g., "1")
+	Owner            string
+	DID              string
+	ConnectionString string
+	SourceChain      string
+	SourceChainID    string
 }
 
 // ToString returns a human-readable string representation of the ViewRegisteredEvent.
